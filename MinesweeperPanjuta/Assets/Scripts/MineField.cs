f5ae809be903b0713db5641c2b24d72d35ed8e74
NicolasWiedel <nicolaswiedel@googlemail.com>
2024-03-15 23:58:11 +0100

    42. first tiles

--- MinesweeperPanjuta/Assets/Scripts/MineField.cs
+++ MinesweeperPanjuta/Assets/Scripts/MineField.cs
@@ -4,10 +4,47 @@ using UnityEngine;
 
 public class MineField : MonoBehaviour
 {
+    private bool isFirstClick;
+    private int amountMines;
+
+    private Tile[,] tiles;
+
+    public int xTotal;
+    public int yTotal;
+
+    public void createTileField(int x, int y, int mines)
+    {
+        isFirstClick = true;
+
+        amountMines= mines;
+        xTotal = x;
+        yTotal = y;
+
+        tiles = new Tile[xTotal, yTotal];
+        for(int i = 0; i < xTotal; i++)
+        {
+            for(int j = 0; j < yTotal; j++)
+            {
+                tiles[i, j] = GetNewTile(i, j);
+            }
+        }
+    }
+
+    private Tile GetNewTile(int x, int y)
+    {
+        GameObject tile = (GameObject)Instantiate(Resources.Load("Prefabs/Tile"));
+        tile.transform.parent = this.gameObject.transform;
+
+        Tile script = tile.GetComponent<Tile>();
+        script.ChangeCoordinates(x, y);
+
+        return script;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        createTileField(20, 20, 10);
     }
 
     // Update is called once per frame
--- MinesweeperPanjuta/Assets/Scripts/Tile.cs
+++ MinesweeperPanjuta/Assets/Scripts/Tile.cs
@@ -4,15 +4,88 @@ using UnityEngine;
 
 public class Tile : MonoBehaviour
 {
+    public bool isMine;
+
+    public Sprite defaultSprite;
+    public Sprite[] emptyFieldSprite;
+    public Sprite mineSprite;
+    public Sprite rescuedFieldSprite;
+    public Sprite deadlyMineSprite;
+    public Sprite rescuedMineSprite;
+
+    public int xCoordinate;
+    public int yCoordinate;
+
+    public bool isRevealed;
+
     // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ChangeSpriteToDefault()
     {
-        
+        GetComponent<SpriteRenderer>().sprite = defaultSprite;
+        GetComponent<BoxCollider2D>().enabled = true;
+    }
+
+    public void ChangeSpriteToEmpty(int i)
+    {
+        GetComponent<SpriteRenderer>().sprite = emptyFieldSprite[i];
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void ChangeSpriteToMine()
+    {
+        GetComponent<SpriteRenderer>().sprite = mineSprite;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void ChangeSpriteToDeadlyMine()
+    {
+        GetComponent<SpriteRenderer>().sprite = deadlyMineSprite;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void ChangeSpriteToRescuedMine()
+    {
+        GetComponent<SpriteRenderer>().sprite = rescuedMineSprite;
+        GetComponent<BoxCollider2D>().enabled = false;
+    }
+
+    public void ToggleRescuedSprite()
+    {
+        if(GetComponent<SpriteRenderer>().sprite == defaultSprite) 
+        {
+            ChangeSpriteToRescuedMine();
+            // TODO: Reduce amount of mines
+        }
+        else if(GetComponent<SpriteRenderer>().sprite == rescuedFieldSprite)
+        {
+            ChangeSpriteToDefault();
+            // TODO; increase amount of mines
+        }
+    }
+
+    public void ChangeCoordinates(int x, int y)
+    {
+        xCoordinate = x;
+        yCoordinate = y;
+
+        transform.position = new Vector2(x, y);
+    }
+
+    private void OnMouseUpAsButton()
+    {
+        // TODO: GameController funktionatity 
+    }
+
+    private void OnMouseOver()
+    {
+        if(Input.GetMouseButtonDown(1)) 
+        { 
+            ToggleRescuedSprite();
+        }
     }
 }
